0cf925d2bd01e7d74657c255f4701f8569956420
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-06-11 10:11:34 +0530

    Update maximumSumSubarray.java

--- src/SlidingWindow/maximumSumSubarray.java
+++ src/SlidingWindow/maximumSumSubarray.java
@@ -2,23 +2,24 @@ package SlidingWindow;
 
 public class maximumSumSubarray {
     public static void main(String[] args) {
-        int[] arr={5,4,3,1,7,8};
-        int k=2;
-        System.out.println(findMaximumSumSubArray(arr,k));
+        int[] arr = {5, 4, 3, 1, 7, 8};
+        int k = 2;
+        System.out.println(findMaximumSumSubArray(arr, k));
     }
-    static int findMaximumSumSubArray(int[] arr,int k){
-        int sum=0;
-        int windowSum=0;
+
+    static int findMaximumSumSubArray(int[] arr, int k) {
+        int sum = 0;
+        int windowSum = 0;
 //        First Window SUm
         for (int i = 0; i < k; i++) {
-            windowSum+=arr[i];
+            windowSum += arr[i];
         }
-        sum=windowSum;
-        for (int i = k+1; i <arr.length ; i++) {
-            windowSum+=arr[i];
-            windowSum-=arr[k];
-            sum=Math.max(sum,windowSum);
-            k+=1;
+        sum = windowSum;
+//        Sliding Window
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i];
+            windowSum -= arr[i - k];
+            sum = Math.max(sum, windowSum);
         }
         return sum;
     }
