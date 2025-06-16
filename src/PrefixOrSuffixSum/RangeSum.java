d694aefb968cc0d72052b993c37be58ffb042230
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-06-17 00:39:19 +0530

    Update RangeSum.java

--- src/PrefixOrSuffixSum/RangeSum.java
+++ src/PrefixOrSuffixSum/RangeSum.java
@@ -3,7 +3,11 @@ package PrefixOrSuffixSum;
 public class RangeSum {
     public  static int[] arr;
     public static void main(String[] args) {
-
+        int[] arr={-2,0,3,-5,2,-1};
+        prefixArray(arr);
+        System.out.println(findSum(0,2));
+        System.out.println(findSum(2,5));
+        System.out.println(findSum(0,5));
     }
     static void prefixArray(int[] nums){
         for (int i = 1; i < nums.length; i++) {
