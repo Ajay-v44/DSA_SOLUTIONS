8e4f9d791557f2ec135eae25567fcb87c6985e95
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-08-10 15:53:52 +0530

    Create Operators.java

---
+++ src/UdemyJavaSpringBoot/Operators.java
@@ -0,0 +1,14 @@
+package UdemyJavaSpringBoot;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.printf("Addition:%d\n", a + b);
+        System.out.printf("Subtraction:%d\n", a - b);
+        System.out.printf("Multiplication:%d\n", a * b);
+        System.out.printf("Division:%d\n", a / b);
+        System.out.printf("Is a greater than b?%b\n", a > b);
+        System.out.printf("Is a > b and b > 0?%b\n", a > b && b > 0);
+    }
+}
