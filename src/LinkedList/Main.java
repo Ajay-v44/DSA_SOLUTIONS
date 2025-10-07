09ca5db19f12e9256a671163ae3f48f606dd56d1
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-07 08:02:33 +0530

    Update Main.java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -8,11 +8,8 @@ public class Main {
         obj.insertAtEnd(50);
         obj.insertAtParticularPosition(510,0);
         obj.printLinkedList();
-<<<<<<< HEAD
-//        obj.deleteNode(0);
+        obj.deleteNode(0);
         obj.deleteNode(2);
         obj.printLinkedList();
-=======
->>>>>>> e2844e805cebfd5e7d3a7598ef39ae995e31db0e
     }
 }
