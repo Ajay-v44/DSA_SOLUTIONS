8c1f836854877efcd2ec9c0db0311cdedc0fc495
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-10 17:17:16 +0530

    Refactor mergeTwoLists to use iterative approach
    Simplified the mergeTwoLists method by removing the addNode helper and implementing an efficient iterative merge using a dummy node. This improves readability and performance by directly linking nodes from the input lists.

--- src/LinkedList/leetcode/MergeSortedLists.java
+++ src/LinkedList/leetcode/MergeSortedLists.java
@@ -1,57 +1,24 @@
 package LinkedList.leetcode;
 
 public class MergeSortedLists {
-    private void addNode(ListNode head, ListNode lst) {
-        if (head == null) {
-            head = lst;
-        }
-        ListNode temp = head;
-        while (temp != null) {
-            temp = temp.next;
-        }
-        temp.next = lst;
-    }
-
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = null;
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        if (list1 != null || list2 != null) {
-            while (temp1 != null && temp2 != null) {
-                if (temp1 != null && temp2 != null) {
-                    if (temp1.val == temp2.val) {
-                        ListNode lst = new ListNode(temp1.val);
-
-                        addNode(head, lst);
-
-                    } else if (temp1.val < temp2.val) {
-                        ListNode lst = new ListNode(temp1.val);
-                        addNode(head, lst);
-
-                        temp1 = temp1.next;
-                    } else {
-                        ListNode lst = new ListNode(temp2.val);
-                        addNode(head, lst);
-
-                        temp2 = temp2.next;
-                    }
-                } else if (temp1 == null) {
-                    ListNode lst = new ListNode(temp1.val);
-
-                    addNode(head, lst);
-
-                    temp2 = temp2.next;
-                } else {
-                    ListNode lst = new ListNode(temp1.val);
-
-                    addNode(head, lst);
-
-                    temp1 = temp1.next;
-                }
-            }
+       ListNode dummy=new ListNode(-1);
+       ListNode current=dummy;
+       while (list1!=null&&list2!=null){
+           if(list1.val<list2.val){
+               current.next=list1;
+               list1=list1.next;
+           }else {
+               current.next=list2;
+               list2=list2.next;
+           }
+           current=current.next;
+       }
+        if(list1!=null){
+            current.next=list1;
+        }else if(list2!=null){
+            current.next=list2;
         }
-
-
-        return head;
+       return dummy.next;
     }
 }
