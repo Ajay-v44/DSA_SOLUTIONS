886ecdb9fb2e7f670466657d81564349a6e7bd87
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-11 16:35:18 +0530

    Add deleteNode method and improve circular list ops
    Implemented the deleteNode method for the circular linked list, allowing deletion by position or index. Improved insertNode logic for index-based insertion, fixed traversal and boundary checks, and enhanced printLinkedList for better output. Updated Main.java to demonstrate new insert and delete operations.

--- src/LinkedList/Circular/CicularLkstOps.java
+++ src/LinkedList/Circular/CicularLkstOps.java
@@ -15,23 +15,24 @@ public class CicularLkstOps {
             head = tail = newNode;
             head.prev = tail;
             tail.next = head;
-        }
-//        else if (head == tail) {
-//            head.next = newNode;
-//            newNode.prev = head;
-//            newNode.next = head;
-//            head.prev = newNode;
-//        }
-        else {
+        } else {
             if (idx.isPresent()) {
+                if (idx.get() <= 1) {
+                    System.out.println("Not Possible should be greater than 1");
+                    return;
+                }
                 Node temp = head;
                 int index = 0;
-                while (temp.next != head && idx.get() < index - 1) {
-                    temp = temp.next;
+                while (temp != null && index < idx.get() - 1) {
+                    if (temp == tail)
+                        temp = null;
+                    else
+                        temp = temp.next;
                     index += 1;
                 }
                 if (temp == null || temp.next == null) {
                     System.out.println("Out Of Bound");
+                    return;
                 }
                 newNode.next = temp.next;
                 temp.next = newNode;
@@ -47,26 +48,55 @@ public class CicularLkstOps {
                 newNode.next = head;
                 newNode.prev = tail;
                 tail.next = newNode;
-                head.prev=newNode;
+                head.prev = newNode;
                 tail = newNode;
             }
         }
     }
 
-//    Display
-    public void printLinkedList(){
-        if(head==tail){
-            System.out.println(head.data);
+    //    Delete Node
+    public void deleteNode(Position ps, Optional<Integer> idx) {
+        if (head == null) {
+            System.out.println("Linked list is empty");
             return;
         }
-        Node temp=head;
-        while (temp!=tail){
-            if(temp==tail)
-                System.out.println(temp.data);
-            else
-                System.out.print("=> "+temp.data);
-            temp=temp.next;
+        if (idx.isPresent()) {
+            Node temp = head;
+            int index = 0;
+            while (temp != null && index < idx.get() - 1) {
+                temp = temp == tail ? null : temp.next;
+                index += 1;
+            }
+            if (temp == null) {
+                System.out.println("Out Of Bound");
+                return;
+            }
+            temp.next = temp.next.next;
+            temp.next.prev = temp;
+        } else if (ps.equals(Position.Head)) {
+            head = head.next;
+            head.prev = tail;
+            tail.next = head;
+        } else {
+            tail = tail.prev;
+            tail.next = head;
+            head.prev = tail;
         }
+    }
+
+    //    Display
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            if (temp == tail) {
+                System.out.print(" =>" + temp.data);
+                temp = null;
+            } else {
+                System.out.print("=> " + temp.data);
+                temp = temp.next;
+            }
+        }
+        System.out.println();
 
     }
 }
--- src/LinkedList/Circular/Main.java
+++ src/LinkedList/Circular/Main.java
@@ -6,11 +6,15 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        CicularLkstOps obj=new CicularLkstOps();
+        CicularLkstOps obj = new CicularLkstOps();
         obj.insertNode(10, Position.Head, java.util.Optional.empty());
         obj.insertNode(15, Position.Head, java.util.Optional.empty());
         obj.insertNode(20, Position.Head, java.util.Optional.empty());
-
+        obj.insertNode(3, Position.Tail, java.util.Optional.empty());
+        obj.insertNode(13, Position.Tail, java.util.Optional.empty());
+        obj.insertNode(600, Position.Tail, Optional.of(3));
+        obj.printLinkedList();
+        obj.deleteNode(Position.Head, Optional.of(3));
         obj.printLinkedList();
     }
 }
