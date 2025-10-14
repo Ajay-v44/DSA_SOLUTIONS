16b1ccc9190ed89b414184e221522c15e1457dc9
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-14 20:29:39 +0530

    LeetCode 234. Palindrome Linked List
    TC - O(n)
    SC - O(1)

--- src/LinkedList/leetcode/Main.java
+++ src/LinkedList/leetcode/Main.java
@@ -9,9 +9,15 @@ public class Main {
         ListNode obj4 = new ListNode(2);
         obj2.next = obj3;
         obj3.next = obj4;
-        obj4.next = obj2;
-        CycleInLinkeList lst = new CycleInLinkeList();
-        System.out.println(lst.hasCycleOpt(obj));
+        ListNode obj5 = new ListNode(5);
+        ListNode obj6 = new ListNode(6);
+        obj2.next = obj5;
+        obj5.next = obj6;
+//        obj4.next = obj2;
+//        CycleInLinkeList lst = new CycleInLinkeList();
+//        System.out.println(lst.hasCycleOpt(obj));
+        Palindrome objP=new Palindrome();
+        System.out.println(objP.isPalindrome(obj));
 
     }
 }
