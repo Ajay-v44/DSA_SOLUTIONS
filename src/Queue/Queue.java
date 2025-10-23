2dd5a6bc7e980115c73e46ebc216c154e91356c2
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-23 08:45:32 +0530

    circular queue

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -1,18 +1,44 @@
 package Queue;
 
+import Queue.Circular.CircularQueue;
+
 public class Queue {
     public static void main(String[] args) {
-        ArrayQueue<Integer> queue=new ArrayQueue<Integer>(5);
-//        System.out.println(queue.deQueue());
-        queue.enQueue(1);
-        queue.enQueue(2);
-        queue.enQueue(3);
-        queue.enQueue(4);
-        queue.enQueue(5);
+//        ArrayQueue<Integer> queue=new ArrayQueue<Integer>(5);
+////        System.out.println(queue.deQueue());
+//        queue.enQueue(1);
+//        queue.enQueue(2);
+//        queue.enQueue(3);
+//        queue.enQueue(4);
+//        queue.enQueue(5);
+//        queue.display();
+//        queue.deQueue();
+//        queue.deQueue();
+//        System.out.println("\n");
+//        queue.display();
+
+
+        CircularQueue queue=new CircularQueue(5);
+        System.out.println(queue.deQueue());
         queue.display();
-        queue.deQueue();
-        queue.deQueue();
-        System.out.println("\n");
+
+        queue.enQueue(5);
+        queue.enQueue(55);
+        queue.enQueue(555);
+        queue.enQueue(5555);
+        queue.display();
+        System.out.println();
+        System.out.println(queue.deQueue());
+        System.out.println();
+        queue.display();
+        queue.enQueue(55555);
+        queue.enQueue(555555);
+        queue.display();
+        System.out.println(queue.deQueue());
+        System.out.println();
+        queue.display();
+        queue.enQueue(44);
+        queue.enQueue(555555);
         queue.display();
     }
 }
