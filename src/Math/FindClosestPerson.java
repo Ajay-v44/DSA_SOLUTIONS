1fc4c542851ede930e88a8b4bd602e771ae5b7fd
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-10-31 14:09:44 +0530

    Leetcode 3516. Find Closest Person

---
+++ src/Math/FindClosestPerson.java
@@ -0,0 +1,16 @@
+package Math;
+
+public class FindClosestPerson {
+    public static int findClosest(int x, int y, int z) {
+//        Absolute removes -ve value ensures result is always +ve
+        int d1=Math.abs(x-z);
+        int d2=Math.abs(y-z);
+        if(d1<d2) return 1;
+        if(d2<d1) return 2;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findClosest(2,7,4));
+    }
+}
