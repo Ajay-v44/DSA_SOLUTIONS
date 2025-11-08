8ce3bab37df50da20abd0ef1a987e27909ed9581
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-11-08 22:08:55 +0530

    Update LevelOrderTraversal.java

--- src/Tree/LevelOrderTraversal.java
+++ src/Tree/LevelOrderTraversal.java
@@ -91,6 +91,7 @@ public class LevelOrderTraversal {
 //        return list;
 //    }
 
+    
     public static void main(String[] args) {
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Node root =BinaryTree.buildBinaryTree(nodes);
