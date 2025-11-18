8c159afaeaa94ca853b201ff7f6c0146229873e5
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2025-11-18 09:46:22 +0530

    Add LongestCommonPrefix and update GraphUtil
    Added a new LongestCommonPrefix class with a method to find the longest common prefix in an array of strings. Updated GraphUtil to support weighted edges, added more edges in createGraphMan, and improved main to print neighbors of vertex 0.

--- src/Graph/GraphUtil.java
+++ src/Graph/GraphUtil.java
@@ -4,24 +4,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GraphUtil {
-    static class Edge{
+    static class Edge {
         int source;
         int destination;
+        int weight;
 
         public Edge(int source, int destination) {
             this.source = source;
             this.destination = destination;
         }
-    }
-    public static void createGraphMan(ArrayList<Edge>[] graph){
-        for (int i = 0; i < graph.length; i++) {
-            graph[i]=new ArrayList<>();
-        }
-        graph[0].add(new Edge(0,1));
-    }
-    public static void main(String[] args) {
-        int V=4;
-        ArrayList<Edge>[] graph =new ArrayList[V];
 
+        public Edge(int source, int destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    public static void createGraphMan(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1,0,-1));
+
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraphMan(graph);
+//        neighbours of vertex 0
+        for (int i = 0; i < graph[0].size(); i++) {
+            Edge edge = graph[0].get(i);
+            System.out.println(edge.destination);
+        }
     }
 }
---
+++ src/String/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+       if (strs==null||strs.length==0)
+                return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(str));
+    }
+}
