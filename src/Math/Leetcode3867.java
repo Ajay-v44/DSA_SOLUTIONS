db865edbd8aa29c18fe15ccd0c5da6ffb9b38794
Ajay-v44 <vajay4834@gmail.com>
2026-07-16 15:47:12 +0530

    Create Leetcode3867.java

---
+++ src/Math/Leetcode3867.java
@@ -0,0 +1,29 @@
+package Math;
+
+import java.util.Arrays;
+
+public class Leetcode3867 {
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public long gcdSum(int[] A) {
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            max = Math.max(max, A[i]);
+            A[i] = gcd(A[i], max);
+        }
+
+        Arrays.sort(A);
+
+        long res = 0;
+        for (int i = 0, j = A.length - 1; i < j; i++, j--)
+            res += gcd(A[i], A[j]);
+
+        return res;
+    }
+
+    static void main() {
+
+    }
+}
