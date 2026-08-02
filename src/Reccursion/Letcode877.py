c43680eab913370c0b26a07790a4a81aa3a5a616
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2026-08-02 13:15:36 +0530

    Create Letcode877.py

---
+++ src/Reccursion/Letcode877.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def stoneGame(self, nums):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        memo={}
+
+        def helper(i,j):
+            if i>j:
+                return 0
+            if i==j:
+                return nums[i]
+
+            if (i,j) in memo:
+                return memo[(i,j)]
+            # Choice 1: Pick left (nums[i]) and subtract what opponent gets from remaining (i+1, j)
+            pick_left = nums[i] - helper(i + 1, j)
+
+            # Choice 2: Pick right (nums[j]) and subtract what opponent gets from remaining (i, j-1)
+            pick_right = nums[j] - helper(i, j - 1)
+
+            memo[(i, j)] = max(pick_left, pick_right)
+            return memo[(i, j)]
+
+        # If Player 1's net score difference is >= 0, they win or tie.
+        return helper(0, len(nums) - 1) >= 0
+        
